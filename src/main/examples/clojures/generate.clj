88d2252005364fbf6607558151ba7b991c0370a3
aristotll <aristotll.good@gmail.com>
2023-02-15 22:27:11 +0800

    not working2

--- src/main/examples/clojures/generate.clj
+++ src/main/examples/clojures/generate.clj
@@ -1,21 +1,25 @@
-;    property(name: 'SELECTION', type: 'com.intellij.util.containers.JBIterable<com.intellij.database.model.DasObject>', doc: 'the selected object')
-(clojure.core/declare ^com.intellij.util.containers.JBIterable SELECTION)
-(clojure.core/declare ^com.intellij.database.extensions.Clipboard CLIPBOARD)
-(clojure.core/declare ^com.intellij.database.extensions.ScriptLogger LOG)
-(clojure.core/declare ^com.intellij.database.extensions.Files FILES)
-(clojure.core/declare ^com.intellij.openapi.project.Project PROJECT)
-(clojure.core/declare ^com.intellij.database.dialects.DatabaseDialect DIALECT)
-(clojure.core/declare ^com.intellij.database.psi.DbTableImpl TABLE)
-;property(name: "ALL_COLUMNS", type: "java.util.List<com.intellij.database.extensions.DataColumn>")
-;    //    public static final Binding<List<? extends DataColumn>> ALL_COLUMNS = new Binding("ALL_COLUMNS");
-(clojure.core/declare ^java.util.List ALL_COLUMNS)
-;//    public static final Binding<List<? extends DataColumn>> COLUMNS = new Binding("COLUMNS");
-;    //    public static final Binding<List<? extends DataColumn>> COLUMNS = new Binding("COLUMNS");
-(clojure.core/declare ^java.util.List COLUMNS)
-(clojure.core/declare ^com.intellij.database.extensions.ValueFormatter FORMATTER)
-(clojure.core/declare ^java.lang.Appendable OUT)
-(clojure.core/declare ^com.intellij.database.extensions.DataStream ROWS)
-(clojure.core/declare ^java.lang.Boolean TRANSPOSED)
+(ns examples.clojures.generate
+  (:require [clojure.core :refer :all])
+  )
+;;    property(name: 'SELECTION', type: 'com.intellij.util.containers.JBIterable<com.intellij.database.model.DasObject>', doc: 'the selected object')
+;(def ^com.intellij.util.containers.JBIterable selection SELECTION)
+;(def ^com.intellij.database.extensions.Clipboard clipboard CLIPBOARD)
+;(def ^com.intellij.database.extensions.ScriptLogger log LOG)
+;;(def ^com.intellij.database.extensions.Files files FILES)
+;;(def files FILES)
+;(def ^com.intellij.openapi.project.Project project PROJECT)
+;(def ^com.intellij.database.dialects.DatabaseDialect dialect DIALECT)
+;(def ^com.intellij.database.psi.DbTableImpl table TABLE)
+;;property(name: "ALL_COLUMNS", type: "java.util.List<com.intellij.database.extensions.DataColumn>")
+;;    //    public static final Binding<List<? extends DataColumn>> ALL_COLUMNS = new Binding("ALL_COLUMNS");
+;(def ^java.util.List ALL_COLUMNS)
+;;//    public static final Binding<List<? extends DataColumn>> COLUMNS = new Binding("COLUMNS");
+;;    //    public static final Binding<List<? extends DataColumn>> COLUMNS = new Binding("COLUMNS");
+;(def ^java.util.List COLUMNS)
+;(def ^com.intellij.database.extensions.ValueFormatter FORMATTER)
+;(def ^java.lang.Appendable OUT)
+;(def ^com.intellij.database.extensions.DataStream ROWS)
+;(def ^java.lang.Boolean TRANSPOSED)
 
 (def usings "using System;")
 (def default-type "string")
@@ -29,7 +33,7 @@
    [["int" "integer"] "int"]
    [["bigint"] "long"]
    [["char"] "char"]
-   [["varbinary" "image"] "byte[]" true] ; cannot be null
+   [["varbinary" "image"] "byte[]" true]                    ; cannot be null
    [["double" "float" "real"] "double"]
    [["decimal" "money" "numeric" "smallmoney"] "decimal"]
    [["datetime" "datetime2" "timestamp" "date" "time"] "DateTime"]
@@ -38,51 +42,51 @@
 (def new-line "\r\n")
 
 (defn- poco-name [name]
-       (apply str (map clojure.string/capitalize (re-seq #"(?:[A-Z]+)?[a-z\d]*" name))))
+  (apply str (map clojure.string/capitalize (re-seq #"(?:[A-Z]+)?[a-z\d]*" name))))
 
 (defn- poco-type [data-type is-null]
-       (let [spec                (.. data-type getSpecification toLowerCase)
-             spec-matches?       (fn [pattern] (= (re-find #"^\w+" spec) pattern))
-             mapping-matches?    (fn [[ps t n]] (when (some spec-matches? ps) [t n]))
-             [type cant-be-null]  (some mapping-matches? type-mappings)
-             nullable-type       (if (and type (not cant-be-null) is-null) (str type "?") type)]
-            (or nullable-type default-type)))
+  (let [spec (.. data-type getSpecification toLowerCase)
+        spec-matches? (fn [pattern] (= (re-find #"^\w+" spec) pattern))
+        mapping-matches? (fn [[ps t n]] (when (some spec-matches? ps) [t n]))
+        [type cant-be-null] (some mapping-matches? type-mappings)
+        nullable-type (if (and type (not cant-be-null) is-null) (str type "?") type)]
+    (or nullable-type default-type)))
 
 (defn- field-infos [table]
-       (let [columns    (com.intellij.database.util.DasUtil/getColumns table)
-             field-info (fn [column] {:name (poco-name (.getName column))
-                                      :type (poco-type (.getDataType column) (not (.isNotNull column)))})]
-            (map field-info columns)))
+  (let [columns (com.intellij.database.util.DasUtil/getColumns table)
+        field-info (fn [column] {:name (poco-name (.getName column))
+                                 :type (poco-type (.getDataType column) (not (.isNotNull column)))})]
+    (map field-info columns)))
 
 (defn- property-text [field-info]
-       (let [type  (:type field-info)
-             name  (:name field-info)]
-            (str "  public " type " " name " { get; set; } " new-line)))
+  (let [type (:type field-info)
+        name (:name field-info)]
+    (str "  public " type " " name " { get; set; } " new-line)))
 
 (defn- poco-text [class-name fields]
-       (apply str (flatten
-                    [usings new-line new-line
-                     "public class " class-name " " new-line "{" new-line
-                     (interpose new-line (interleave (map property-text fields)))
-                     "}" new-line])))
+  (apply str (flatten
+               [usings new-line new-line
+                "public class " class-name " " new-line "{" new-line
+                (interpose new-line (interleave (map property-text fields)))
+                "}" new-line])))
 
 (defn- generate-poco [directory table]
-       (let [class-name (poco-name (.getName table))
-             fields     (field-infos table)
-             file       (java.io.File. directory (str class-name ".cs"))
-             text       (poco-text class-name fields)]
-            (com.intellij.openapi.util.io.FileUtil/writeToFile file text)))
+  (let [class-name (poco-name (.getName table))
+        fields (field-infos table)
+        file (java.io.File. directory (str class-name ".cs"))
+        text (poco-text class-name fields)]
+    (com.intellij.openapi.util.io.FileUtil/writeToFile file text)))
 
 (defn- generate-pocos [directory]
-       (let [table? (partial instance? com.intellij.database.model.DasTable)]
-            (doseq [table (filter table? SELECTION)]
-                   (generate-poco directory table))))
+  (let [table? (partial instance? com.intellij.database.model.DasTable)]
+    (doseq [table (filter table? SELECTION)]
+      (generate-poco directory table))))
 
 
 (.chooseDirectoryAndSave FILES
                          "Choose directory"
                          "Choose where to generate POCOs to"
                          (proxy [com.intellij.util.Consumer] []
-                                (consume [directory]
-                                         (generate-pocos directory)
-                                         (.refresh FILES directory))))
+                           (consume [directory]
+                             (generate-pocos directory)
+                             (.refresh FILES directory))))
