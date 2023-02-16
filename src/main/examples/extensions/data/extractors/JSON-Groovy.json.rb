6c571072325d6a0473309950bcb35b8b5f2e7167
aristotll <aristotll.good@gmail.com>
2023-02-16 23:26:02 +0800

    exception is thrown

--- src/main/examples/extensions/data/extractors/JSON-Groovy.json.rb
+++ src/main/examples/extensions/data/extractors/JSON-Groovy.json.rb
@@ -1,53 +1,25 @@
-# import static com.intellij.openapi.util.text.StringUtil.escapeStringCharacters as escapeStr
 java_import "com.intellij.openapi.util.text.StringUtil"
+import com.intellij.database.model.DasObject
 NEWLINE = java.lang.System.getProperty("line.separator")
-# INDENT = "  "
-#
-# def printJSON(level, col, o) {
-#   switch (o) {
-#     case null: OUT.append("null"); break
-#     case Tuple: printJSON(level, o[0], o[1]); break
-#     case Map:
-#       OUT.append("{")
-#       o.entrySet().eachWithIndex { entry, i ->
-#         OUT.append("${i > 0 ? "," : ""}$NEWLINE${INDENT * (level + 1)}")
-#         OUT.append("\"${escapeStr(entry.getKey().toString())}\"")
-#         OUT.append(": ")
-#         printJSON(level + 1, col, entry.getValue())
-#       }
-#       OUT.append("$NEWLINE${INDENT * level}}")
-#       break
-#     case Object[]:
-#     case Iterable:
-#       OUT.append("[")
-#       def plain = true
-#       o.eachWithIndex { item, i ->
-#         plain = item == null || item instanceof Number || item instanceof Boolean || item instanceof String
-#         if (plain) {
-#           OUT.append(i > 0 ? ", " : "")
-#         }
-#         else {
-#           OUT.append("${i > 0 ? "," : ""}$NEWLINE${INDENT * (level + 1)}")
-#         }
-#         printJSON(level + 1, col, item)
-#       }
-#       if (plain) OUT.append("]") else OUT.append("$NEWLINE${INDENT * level}]")
-#       break
-#     case Boolean: OUT.append("$o"); break
-#     default:
-#       def str = FORMATTER.formatValue(o, col)
-#       OUT.append(FORMATTER.isStringLiteral(o, col) ? "\"${escapeStr(str)}\"" : str);
-#       break
-#   }
-# }
-#
-# printJSON(0, null, ROWS.transform { row ->
-#   def map = new LinkedHashMap<String, String>()
-#   COLUMNS.each { col ->
-#     if (row.hasValue(col)) {
-#       def val = row.value(col)
-#       map.put(col.name(), new Tuple(col, val))
-#     }
-#   }
-#   map
-# })
+
+# the logic is very simple even the groovy implementation is very complicated
+
+# create a list of map to represent the selection
+# convert it to json string
+
+result = ROWS.to_a.collect do
+  # @type [DasObject] row
+|row|
+  COLUMNS.map do
+  |column|
+    #         if (row.hasValue(col)) {
+    #             def val = row.value(col)
+    #             map[col.name()] = new Tuple(col, val)
+    #         }
+    row.has_value(column) ? [column.name, row.value(column)] : nil
+  end.compact.to_h
+end
+
+OUT.append(
+  (result.size() == 1 ? result[0] : result).to_json
+)
